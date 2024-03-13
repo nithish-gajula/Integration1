b75d491605d66962c6b6a51a0a84a090d6b9f2f5
nithish-gajula <gajulanithish000@gmail.com>
2024-03-13 23:53:41 +0530

    Started Charts Theory

--- app/src/main/java/com/example/integration1/StatisticsFragment.kt
+++ app/src/main/java/com/example/integration1/StatisticsFragment.kt
@@ -13,6 +13,16 @@ class StatisticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        /*************************************
+         * Implement following Charts, Also try Material Dat Visualization
+         * Pie Chart, Bar Chart, Radar Chart
+         * -----------Specific -------------
+         * Specific Person - Specific Month
+         * Specific Person - Overall
+         * -----------Overall --------------
+         * Everyone - Specific Month
+         * Everyone - Overall
+         **************************************/
         return inflater.inflate(R.layout.fragment_statistics, container, false)
     }
 }
